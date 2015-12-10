30ae2a26e7b267a0843f4632e2559223ad6cbb3f
Paulus <paulus@gewirr.cc>
2015-12-10 19:12:32 +0300

    append submodules

---
+++ elpa/jenkins-watch-20121004.1626/jenkins-watch-autoloads.el
@@ -0,0 +1,16 @@
+;;; jenkins-watch-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("jenkins-watch.el") (22119 28669 224643
+;;;;;;  242000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jenkins-watch-autoloads.el ends here
---
+++ elpa/jenkins-watch-20121004.1626/jenkins-watch-pkg.el
@@ -0,0 +1,1 @@
+(define-package "jenkins-watch" "20121004.1626" "Watch continuous integration build status" 'nil :url "https://github.com/ataylor284/jenkins-watch")
